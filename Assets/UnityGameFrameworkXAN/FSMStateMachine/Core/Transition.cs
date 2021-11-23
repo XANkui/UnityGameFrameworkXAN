62fced68cd511dfe66c4ec6f01a0d10e23ae2291
xan <xan.qin@madgaze.com>
2021-11-23 20:49:13 +0800

    游戏框架添加有限状态机功能

---
+++ Assets/UnityGameFrameworkXAN/FSMStateMachine/Core/Transition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XANFSM {
+
+	/// <summary>
+	/// 过渡 ID 
+	/// 可以根据需要添加多种 过渡 ID
+	/// </summary>
+	public enum Transition 
+	{
+		None,
+
+		// NPC 
+		SeePlayer,
+		LostPlayer,
+
+		// UI 
+	}
+}
