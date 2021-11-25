346c0230819cfc100300ed11f3e7b85560bf527e
xan <xan.qin@madgaze.com>
2021-11-25 17:43:00 +0800

    “添加单例类”

---
+++ Assets/UnityGameFrameworkXAN/Singleton/Core/MonoSingleton.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+namespace UnityGameFrameworkXAN {
+
+   
+    /// <summary>
+    /// Mono 单例类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T instance = null;
+
+        private static readonly object locker = new object();
+
+        private static bool mIsAppQuitting;
+
+        public static T Instance
+        {
+            get
+            {
+                // 脚本销毁则单例置空
+                if (mIsAppQuitting)
+                {
+                    instance = null;
+                    return instance;
+                }
+
+                // 线程安全处理
+                lock (locker)
+                {
+                    if (instance == null)
+                    {
+                        // 保证场景中只有一个 单例
+                        T[] managers = Object.FindObjectsOfType(typeof(T)) as T[];
+                        if (managers.Length != 0)
+                        {
+                            if (managers.Length == 1)
+                            {
+                                instance = managers[0];
+                                instance.gameObject.name = typeof(T).Name;
+                                return instance;
+                            }
+                            else
+                            {
+                                Debug.LogError("Class " + typeof(T).Name + " exists multiple times in violation of singleton pattern. Destroying all copies");
+                                foreach (T manager in managers)
+                                {
+                                    Destroy(manager.gameObject);
+                                }
+                            }
+                        }
+
+
+                        var singleton = new GameObject();
+                        instance = singleton.AddComponent<T>();
+                        singleton.name = "(singleton)" + typeof(T);
+                        singleton.hideFlags = HideFlags.None;
+                        DontDestroyOnLoad(singleton);
+
+                    }
+                    instance.hideFlags = HideFlags.None;
+                    return instance;
+                }
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            mIsAppQuitting = false;
+        }
+
+        protected virtual void OnDestroy()
+        {
+            mIsAppQuitting = true;
+        }
+    }
+}
---
+++ Assets/UnityGameFrameworkXAN/Singleton/TestSingleton/TestSingleton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityGameFrameworkXAN.Test
+{ 
+
+	public class TestSingleton : MonoSingleton<TestSingleton>
+	{
+		// Start is called before the first frame update
+		void Start()
+		{
+			
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			
+		}
+	}
+
+	public class TestClass : Singleton<TestClass> {
+		// 注意：添加非公有无参构造函数，不然会报错
+		protected TestClass() { }
+	}
+}
